a351c3fd869292c05cebc49b5b1403bdc6a9f11d
Sharon-liu-go <innovate72095@gmail.com>
2021-01-29 11:42:05 +0800

    create newebpat_helpers.js and move the related function from orderController to it

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,106 +1,8 @@
 const db = require('../models')
 const { Cart, CartItem, Product, Order, OrderItem } = db
 const nodemailer = require('nodemailer');
-const crypto = require("crypto")
+const newebpay_helpers = require('../config/newebpay-helpers')
 
-const URL = 'http://f2ab5855094a.ngrok.io'
-const MerchantID = 'MS318058038'
-const HashKey = 'x4C7XNUFDuEQsBCcM8Pdgf2sO9pni1Ji'
-const HashIV = 'CJXd3i4Rvm2Fol3P'
-const PayGateWay = "https://ccore.newebpay.com/MPG/mpg_gateway"
-const ReturnURL = URL + "/newebpay/callback?from=ReturnURL"
-const NotifyURL = URL + "/newebpay/callback?from=NotifyURL"
-const ClientBackURL = URL + "/orders"
-
-//取得付款方式的參數
-function getPayMethod(method) {
-  switch (method) {
-    case '1': return 'CREDIT'
-  }
-}
-
-function genDataChain(TradeInfo) {
-  let results = [];
-  for (let kv of Object.entries(TradeInfo)) {
-    results.push(`${kv[0]}=${kv[1]}`);
-  }
-  return results.join("&");
-}
-
-function create_mpg_aes_encrypt(TradeInfo) {
-  let encrypt = crypto.createCipheriv("aes256", HashKey, HashIV);
-  let enc = encrypt.update(genDataChain(TradeInfo), "utf8", "hex");
-  return enc + encrypt.final("hex");
-}
-
-function create_mpg_aes_decrypt(TradeInfo) {
-  let decrypt = crypto.createDecipheriv("aes256", HashKey, HashIV);
-  decrypt.setAutoPadding(false);
-  let text = decrypt.update(TradeInfo, "hex", "utf8");
-  let plainText = text + decrypt.final("utf8");
-  let result = plainText.replace(/[\x00-\x20]+/g, "");
-  return result;
-}
-
-
-function create_mpg_sha_encrypt(TradeInfo) {
-
-  let sha = crypto.createHash("sha256");
-  let plainText = `HashKey=${HashKey}&${TradeInfo}&HashIV=${HashIV}`
-
-  return sha.update(plainText).digest("hex").toUpperCase();
-}
-
-function getTradeInfo(Amt, Desc, email, paymentMethod) {
-
-  console.log('===== getTradeInfo =====')
-  console.log(Amt, Desc, email, paymentMethod)
-  console.log('==========')
-
-  data = {
-    'MerchantID': MerchantID, // 商店代號
-    'RespondType': 'JSON', // 回傳格式
-    'TimeStamp': Date.now(), // 時間戳記
-    'Version': 1.5, // 串接程式版本
-    'MerchantOrderNo': Date.now(), // 商店訂單編號
-    'LoginType': 0, // 智付通會員
-    'OrderComment': 'OrderComment', // 商店備註
-    'Amt': Amt, // 訂單金額
-    'ItemDesc': Desc, // 產品名稱
-    'Email': email, // 付款人電子信箱
-    'ReturnURL': ReturnURL, // 支付完成返回商店網址
-    'NotifyURL': NotifyURL, // 支付通知網址/每期授權結果通知
-    'ClientBackURL': ClientBackURL, // 支付取消返回商店網址
-  }
-  //加入paymentMethod的參數，啟用:1
-
-  data[paymentMethod] = 1
-
-  console.log('===== getTradeInfo: data =====')
-  console.log(data)
-
-
-  mpg_aes_encrypt = create_mpg_aes_encrypt(data)
-  mpg_sha_encrypt = create_mpg_sha_encrypt(mpg_aes_encrypt)
-
-  console.log('===== getTradeInfo: mpg_aes_encrypt, mpg_sha_encrypt =====')
-  console.log(mpg_aes_encrypt)
-  console.log(mpg_sha_encrypt)
-
-  tradeInfo = {
-    'MerchantID': MerchantID, // 商店代號
-    'TradeInfo': mpg_aes_encrypt, // 加密後參數
-    'TradeSha': mpg_sha_encrypt,
-    'Version': 1.5, // 串接程式版本
-    'PayGateWay': PayGateWay,
-    'MerchantOrderNo': data.MerchantOrderNo,
-  }
-
-  console.log('===== getTradeInfo: tradeInfo =====')
-  console.log(tradeInfo)
-
-  return tradeInfo
-}
 
 
 
@@ -167,9 +69,9 @@ let orderController = {
     console.log('==========')
 
     return Order.findByPk(req.params.id, { include: 'items' }).then(order => {
-      let paymentMethod = getPayMethod(order.dataValues.payment_method)
+      let paymentMethod = newebpay_helpers.getPayParam(order.dataValues.payment_method)
 
-      const tradeInfo = getTradeInfo(order.amount, '產品名稱', 'innovate72095@gmail.com', paymentMethod)
+      const tradeInfo = newebpay_helpers.getTradeInfo(order.amount, '產品名稱', 'innovate72095@gmail.com', paymentMethod)
       order.update({
         sn: tradeInfo.MerchantOrderNo,
       }).then(order => {
@@ -194,7 +96,7 @@ let orderController = {
     console.log('===== spgatewayCallback: TradeInfo =====')
     console.log(req.body.TradeInfo)
 
-    const data = JSON.parse(create_mpg_aes_decrypt(req.body.TradeInfo))
+    const data = JSON.parse(newebpay_helpers.create_mpg_aes_decrypt(req.body.TradeInfo))
 
     console.log('===== spgatewayCallback: create_mpg_aes_decrypt、data =====')
     console.log(data)
