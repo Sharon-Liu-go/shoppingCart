8110df1ac558262413ec6786a3460eac78181856
Sharon-liu-go <innovate72095@gmail.com>
2021-02-03 11:45:24 +0800

    update newebpay URL

--- config/newebpay-helpers.js
+++ config/newebpay-helpers.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto")
 
-const URL = 'http://f8f82c9ea6ec.ngrok.io'
+const URL = process.env.NEWEBPAY_URL
 const MerchantID = 'MS318058038'
 const HashKey = 'x4C7XNUFDuEQsBCcM8Pdgf2sO9pni1Ji'
 const HashIV = 'CJXd3i4Rvm2Fol3P'
